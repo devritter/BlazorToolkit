e59071d649a4054ea324128aa5b94e0ded7415d4
David Ritter <devritter@hotmail.com>
2025-01-13 19:16:30 +0100

    FluxorIntegration

---
+++ BlazingDev.BlazorToolkit.FluxorIntegration/FluxorIntegration.cs
@@ -0,0 +1,58 @@
+﻿using BlazingDev.BlazorToolkit.Components.Integrations;
+using Fluxor;
+
+namespace BlazingDev.BlazorToolkit.FluxorIntegration;
+
+public class FluxorIntegration(IActionSubscriber actionSubscriber) : IBzComponentIntegration
+{
+    private BzComponentIntegrationInitializationContext _ctx = null!;
+
+    Task IBzComponentIntegration.InitializeAsync(BzComponentIntegrationInitializationContext ctx)
+    {
+        _ctx = ctx;
+
+        var stateSubscription = StateSubscriber.Subscribe(ctx.Component, _ =>
+        {
+            if (!_ctx.ComponentInternals.IsDisposed)
+            {
+                _ctx.ComponentInternals.InvokeAsync(_ctx.ComponentInternals.StateHasChanged);
+            }
+        });
+
+        // disposing
+        ctx.Disposer.Add(stateSubscription);
+        ctx.Disposer.Add(() => actionSubscriber.UnsubscribeFromAllActions(this));
+
+        return Task.CompletedTask;
+    }
+
+    public void SubscribeToAction<TAction>(Action<TAction> callback)
+    {
+        actionSubscriber.SubscribeToAction<TAction>(this, action =>
+        {
+            _ctx.ComponentInternals.InvokeAsync(() =>
+            {
+                if (!_ctx.ComponentInternals.IsDisposed)
+                {
+                    callback(action);
+                    _ctx.ComponentInternals.StateHasChanged();
+                }
+            });
+        });
+    }
+
+    public void SubscribeToAction<TAction>(Func<TAction, Task> callback)
+    {
+        actionSubscriber.SubscribeToAction<TAction>(this, action =>
+        {
+            _ctx.ComponentInternals.InvokeAsync(async () =>
+            {
+                if (!_ctx.ComponentInternals.IsDisposed)
+                {
+                    await callback(action);
+                    _ctx.ComponentInternals.StateHasChanged();
+                }
+            });
+        });
+    }
+}
